f07c8b09b81947dc1c3ff01d41d630e7daea80a0
mbower00 <mitchbbower@gmail.com>
2024-07-17 15:13:35 -0600

    working on header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,86 @@
 <template>
-  <nav>
-    <router-link to="/">Explore</router-link>
-    <router-link to="/practice">Practice</router-link>
-  </nav>
+  <header class="header-header">
+    <div class="header-title-container">
+      <h1 class="header-title">lexileaf</h1>
+      <div class="header-hamburger-container">
+        <HamburgerButton />
+      </div>
+    </div>
+    <nav class="header-nav">
+      <router-link class="header-link" to="/">Explore</router-link>
+      <router-link class="header-link" to="/practice">Practice</router-link>
+    </nav>
+  </header>
   <router-view />
 </template>
 
+<script>
+import HamburgerButton from "./components/HamburgerButton.vue"
+
+export default {
+  components: {
+    HamburgerButton
+  }
+}
+</script>
+
 <style scoped>
-nav {
-  display: flex;
-  gap: 10px;
+.header-header {
+  display: block;
+  align-items: center;
+  background-color: var(--green2);
+  color: var(--white);
 }
 
-a {
+.header-nav {
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  text-align: center;
+  padding: 0 15px 15px 15px;
+}
+
+.header-hamburger-container {
+  position: absolute;
+  right: 15px;
+  font-size: xx-large;
+}
+
+.header-title-container {
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+
+.header-title {
+  margin: 0;
+  padding: 15px;
+}
+
+.header-link {
   text-decoration: none;
-  color: var(--text);
+  color: var(--white);
 }
 
 .router-link-active {
   text-decoration: underline;
+}
+
+/* using code from https://www.w3schools.com/css/css3_mediaqueries.asp */
+@media (min-width: 426px) {
+  .header-header {
+    display: flex;
+  }
+
+  .header-nav {
+    flex-direction: row;
+    /* using code from https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_flexible_box_layout/Aligning_items_in_a_flex_container */
+    margin-left: auto;
+    padding: 15px;
+  }
+
+  .header-hamburger-container {
+    display: none;
+  }
 }
 </style>
---
+++ src/components/HamburgerButton.vue
@@ -0,0 +1,71 @@
+<template>
+  <!-- using code from https://vuejs.org/guide/essentials/event-handling.html -->
+  <div @click="toggleHamburger" class="hamburger-container">
+    <!-- using code from https://vuejs.org/guide/essentials/class-and-style.html#class-and-style-bindings -->
+    <div :class="{ bar: true, topBar: true, isX: isX }"></div>
+    <div :class="{ bar: true, middleBar: true, isX: isX }"></div>
+    <div :class="{ bar: true, bottomBar: true, isX: isX }"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isX: false
+    }
+  },
+  methods: {
+    toggleHamburger() {
+      this.isX = !this.isX
+      console.log(this.isX)
+    }
+  },
+}
+</script>
+
+<style scoped>
+/* using code from:
+ - https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function/rotate
+ - https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function/translate 
+ - https://www.w3schools.com/cssref/css_selectors.php
+ */
+
+.hamburger-container {
+  position: relative;
+  width: 30px;
+  height: 20px;
+  align-items: center;
+  display: flex;
+}
+
+.bar {
+  background-color: var(--white);
+  width: 30px;
+  height: 3px;
+  position: absolute;
+  transition: all ease-out .3s;
+  transform: rotate(0deg);
+}
+
+.topBar {
+  transform: translate(0, -10px);
+}
+
+
+.bottomBar {
+  transform: translate(0, 10px);
+}
+
+.isX.topBar {
+  transform: rotate(45deg) translate(0, 0);
+}
+
+.isX.middleBar {
+  transform: rotate(-45deg);
+}
+
+.isX.bottomBar {
+  transform: rotate(45deg) translate(0, 0);
+}
+</style>
